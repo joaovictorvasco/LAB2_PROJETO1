b7a15009f1cdd57eb4fa4449492ffba51fa55938
João Victor Sepulveda Penido <joaovictor.gestaodetrafego@gmail.com>
2024-08-09 19:58:50 -0300

    Terceira tarefa, Sorteio de números

--- LAB2_PROJETO1/Arquivo.py
+++ LAB2_PROJETO1/Arquivo.py
@@ -1,4 +1,13 @@
+import random as rd
+
 def cumprimento(texto):
     return f"Olá, {texto}"
 
 print(cumprimento("João Victor Sepulveda Penido"))
+
+def sorteia_media():
+    numeros = [rd.randint(1, 100) for _ in range(3)]
+    media = sum(numeros) / 3
+    return media
+
+print(f"A média dos três números sorteados é: {sorteia_media()}")
